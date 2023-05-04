8e5442ce07c8ad0fbdccaaa7de69e09527252cc8
Stanislau Zakharevich <stanislau.zakharevich@gmail.com>
2023-05-04 13:36:53 +0200

    feat(ExcelReader): made config fields mutable

--- ExcelAssistant/ExcelConfiguration.cs
+++ ExcelAssistant/ExcelConfiguration.cs
@@ -5,27 +5,27 @@ public class ExcelConfiguration
     /// <summary>
     /// Type of Excel file
     /// </summary>
-    public ExcelType ExcelType { get; init; }
+    public ExcelType? ExcelType { get; set; }
     
     /// <summary>
     /// Sheet Name. By default will be reading first sheet.
     /// </summary>
-    public string SheetName { get; init; }
+    public string SheetName { get; set; }
 
     /// <summary>
     /// Set it if you want to read data started not from first column.
     /// </summary>
-    public string MainColumnName { get; init; }
+    public string MainColumnName { get; set; }
     
     /// <summary>
     /// Headers like they displayed in excel file and c# field equivalent
     /// </summary>
-    public Dictionary<string, string> HumanReadableHeaders { get; init; } = new();
+    public Dictionary<string, string> HumanReadableHeaders { get; set; } = new();
 
     /// <summary>
     /// Percent Matching excel headers with c# model fields. 
     /// </summary>
-    public int MatchingPercentage { get; init; } = 80;
+    public int MatchingPercentage { get; set; } = 80;
 
     /// <summary>
     /// The coefficient shows how much space is needed for one symbol.
